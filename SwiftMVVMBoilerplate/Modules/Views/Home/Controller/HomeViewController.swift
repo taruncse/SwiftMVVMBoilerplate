d2aadb476d6e701c9fdff3bf38e52c5474a7eec3
Tarun <tarun.biswas@dreamonline.co.jp>
2022-03-14 01:10:22 +0600

    - setup HomeViewController as initial screen

---
+++ SwiftMVVMBoilerplate/Modules/Views/Home/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  SwiftMVVMBoilerplate
+//
+//  Created by DreamOnline on 14/3/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
